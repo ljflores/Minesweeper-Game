b6a4ee62390e1427d33153bb98cda78778f631a7
Christensen, James T <james-t-christensen@uiowa.edu>
2020-04-28 15:07:38 -0500

    Adding in the Advanced Square rules files

---
+++ AdvSquareRules.h
@@ -0,0 +1,18 @@
+//
+//  AdvSquareRules.h
+//  OOfinalproject
+//
+//  Created by James Christensen on 4/28/20.
+//  Copyright © 2020 James Christensen. All rights reserved.
+//
+#include "Rules.h"
+#ifndef AdvSquareRules_h
+#define AdvSquareRules_h
+class AdvSquareRules : public Rules {
+public:
+    AdvSquareRules();
+    
+    void printRules();
+};
+
+#endif /* AdvSquareRules_h */
