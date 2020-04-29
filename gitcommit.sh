b556809d646c59b74ddfbd7f87f527d7e3948be0
jtchristensen <james-t-christensen@uiowa.edu>
2020-04-29 18:30:00 -0500

    Deleted my scripts to clean everything up
    Signed-off-by: jtchristensen <james-t-christensen@uiowa.edu>

--- gitcommit.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "This is James's getcommit tool for OOSD final project"
-echo "What would you like your comment to be for your commit"
-read mycomment
-git pull origin master
-git add --all
-git commit -m "$mycomment"
-git push origin master
