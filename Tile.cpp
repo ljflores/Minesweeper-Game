649b976fb27b37bdd1032a98ac4b9d9cff376c8e
jtchristensen <james-t-christensen@uiowa.edu>
2020-04-30 14:54:09 -0500

    Added more methods to the tile base class.
    Signed-off-by: jtchristensen <james-t-christensen@uiowa.edu>

---
+++ Tile.cpp
@@ -0,0 +1,8 @@
+//
+// Created by James Christensen on 4/30/20.
+//
+
+#include "Tile.h"
+Tile::Tile(){
+
+}
--- Tile.h
+++ Tile.h
@@ -3,8 +3,14 @@
 
 // An interface that defines what all tiles must do.
 class Tile {
+    string status;
 public:
+    Tile();
     virtual void printTile()=0;
+    virtual void ChangeDisplay(string s)=0;//change the way the tile works
+    bool IsMine()=0;//checks if tile is a mine
+    string GetStatus();//gets status of Tiles
+    SetStatus(string);//sets status of string
 };
 
 #endif //MINESWEEPERBUILDER_TILE_H
